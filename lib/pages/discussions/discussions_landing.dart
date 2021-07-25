117291e9e0ad04bf512da27fdff65382d3e365e5
Vaishnavi <vaishnavis0990@gmail.com>
2021-07-25 15:30:03 +0530

    .

--- lib/pages/discussions/discussions_landing.dart
+++ lib/pages/discussions/discussions_landing.dart
@@ -31,29 +31,31 @@ class _DiscussionsLandingState extends State<DiscussionsLanding> {
               Lottie.asset('assets/discussions.json',
                   width: size.width * 0.35, height: size.height * 0.25),
 
-              Text(
-                  'Let us know if something disturbing took place, we will use this information to make the place safer.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      // your text
+              Center(
+                  child: Container(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      child: Text(
+                          'Let us know if something disturbing took place, we will use this information to make the place safer.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              // your text
 
-                      fontSize: 23.0,
-                      height: 1,
-                      color: Colors.black)),
-
+                              fontSize: 23.0,
+                              height: 1,
+                              color: Colors.black)))),
               ElevatedButton(
                 onPressed: () => {},
                 style: ElevatedButton.styleFrom(
                     primary: Palette.AppBarBackground,
                     padding: EdgeInsets.only(
-                        right: 10.0, bottom: 1.0, top: 1.0, left: 10.0)),
+                        right: 18.0, bottom: 2.0, top: 2.0, left: 15.0)),
                 child: Text(
                   'Go Now',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                      fontSize: 18.0), //TextStyle
+                      fontSize: 23.0), //TextStyle
                 ), //Text
               ),
 
