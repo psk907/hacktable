c0cee4e61fdf4c65fe305146eb6d70df08e6112e
Sankalp Shanbhag <sankalpshanbhag@Sankalps-MacBook-Pro.local>
2021-07-25 20:04:21 +0530

    Incidents updated

--- lib/models/incidents_example.dart
+++ lib/models/incidents_example.dart
@@ -75,5 +75,141 @@ const List incidentsExample = [
     "reportedOn": "2021-25-07 06:22:50",
     "topics": ["stalker", "unsafe", "creepy"],
     "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Anonymous",
+    "location": [13.01252691453853, 77.55383392724337],
+    "incidentSummary":
+    "posing as a tour guide and offering to show me the sights of the city",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 11:32:00",
+    "topics": ["doubtful", "unsafe", "scared"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Anonymous",
+    "location": [12.986430522022989, 77.60105765063312],
+    "incidentSummary": "Slipping sedative drugs into my food or drink.",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 01:30:04",
+    "topics": ["life-threatening", "unsafe", "risky"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Priya",
+    "location": [13.037707142407806, 77.52125059342646],
+    "incidentSummary": "Car is following for the past 20 mins",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 06:22:50",
+    "topics": ["stalker", "unsafe", "creepy"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Nisha",
+    "location": [13.011050, 77.5549665],
+    "incidentSummary":
+    "posing as a tour guide and offering to show me the sights of the city",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 11:32:00",
+    "topics": ["doubtful", "unsafe", "scared"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Arjun",
+    "location": [13.040177603851527, 77.5891652457871],
+    "incidentSummary": "Slipping sedative drugs into my food or drink.",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 01:30:04",
+    "topics": ["life-threatening", "unsafe", "risky"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Anonymous",
+    "location": [13.036204483128921, 77.55152998700514],
+    "incidentSummary": "being followed for the past 30 mins",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 06:22:50",
+    "topics": ["stalker", "unsafe", "creepy"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Anonymous",
+    "location": [12.969289926382652, 77.49182590008385],
+    "incidentSummary":
+    "posing as a tour guide and offering to show me the sights of the city",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 11:32:00",
+    "topics": ["doubtful", "unsafe", "scared"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Anonymous",
+    "location": [12.983022063424686, 77.49556908651925],
+    "incidentSummary": "Slipping sedative drugs into my food or drink.",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 01:30:04",
+    "topics": ["life-threatening", "unsafe", "risky"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Priya",
+    "location": [12.984240451811333, 77.44378337292072],
+    "incidentSummary": "Car is following for the past 20 mins",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 06:22:50",
+    "topics": ["stalker", "unsafe", "creepy"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Nisha",
+    "location": [13.040564434263858, 77.54509046965484],
+    "incidentSummary":
+    "posing as a tour guide and offering to show me the sights of the city",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 11:32:00",
+    "topics": ["doubtful", "unsafe", "scared"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Arjun",
+    "location": [12.987895951508367, 77.42633020658542],
+    "incidentSummary": "Slipping sedative drugs into my food or drink.",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 01:30:04",
+    "topics": ["life-threatening", "unsafe", "risky"],
+    "sentimentIntensity": -0.8
+  },
+  {
+    "userUuid": "",
+    "incidentUuid": "",
+    "userName": "Anonymous",
+    "location": [12.951190686539007, 77.4429040718618],
+    "incidentSummary": "being followed for the past 30 mins",
+    "conversationId": "",
+    "reportedOn": "2021-25-07 06:22:50",
+    "topics": ["stalker", "unsafe", "creepy"],
+    "sentimentIntensity": -0.8
   }
 ];
